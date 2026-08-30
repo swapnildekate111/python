febc5b29131aa161173fd5e1bad614f20fdaa4e5
swapnildekate111 <swapnildekate111@gmail.com>
2026-08-30 10:33:26 +0530

    numeric datatypes

--- Datatypes/datatypes.py
+++ Datatypes/datatypes.py
@@ -4,3 +4,11 @@ x = 60.5    # floating point
 x = "Hello, world!"     # string
 x = ['alice', 'bob', 'charlie']     # list
 
+######## Numberic Data Types ###################
+a = 5       # int
+b = 5.0     # float
+c = 2 + 4j  # complex number
+
+print(type(a))
+print(type(b))
+print(type(c))

bae40f74a927eddcc18951f984afa6294fd9ddf5
swapnildekate111 <swapnildekate111@gmail.com>
2026-09-05 10:37:51 +0530

    Relational Operations

--- operators/operator.py
+++ operators/operator.py
@@ -13,10 +13,21 @@
 a = 15
 b = 4
 
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b) # returns floating(datatype) point value
-print("Floor Division:", a // b) # returns integer(datatype) value
-print("Modulo:", a % b)
-print("Exponentiation:", a ** b)
+# print("Addition:", a + b)
+# print("Subtraction:", a - b)
+# print("Multiplication:", a * b)
+# print("Division:", a / b) # returns floating(datatype) point value
+# print("Floor Division:", a // b) # returns integer(datatype) value
+# print("Modulo:", a % b)
+# print("Exponentiation:", a ** b)
+
+############# Relational Operator #############
+a = 13
+b = 33
+
+print(a > b) # 13 is greater than 33 -> False
+print(a < b) # 13 is less than 33 -> True
+print(a == b) # 13 is equal to 33 -> False
+print(a != b) # 13 is not equal to 33 -> True
+print(a >= b) # 13 is greater than or equal to 33 -> False
+print(a <= b) # 13 is less than or equal to 33 -> True

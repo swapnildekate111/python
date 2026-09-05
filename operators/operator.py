af4425daacc077fa40866a8d0ffbd16382496adc
swapnildekate111 <swapnildekate111@gmail.com>
2026-09-05 10:23:21 +0530

    today's agenda

---
+++ operators/operator.py
@@ -0,0 +1,10 @@
+############################### OPERATORS ###############################
+"""
+# Arithmetic Operator (+, -, *, /, %)
+# Relational Operator (<, >, <=, >=, !=, ==)
+# Logical Operator (AND, OR, NOT)
+# Bitwise Operator (&, |, ^, ~, <<, >>)
+# Assignment Operator (=, +=, -=, *=, /=)
+# Ternary Operator (x if condition else y)
+# Identity Operator (is, is not)
+"""

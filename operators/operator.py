5341cba2578c19f031caded4f9349d2eeb9b9c5f
swapnildekate111 <swapnildekate111@gmail.com>
2026-09-05 10:32:04 +0530

    Arithmetic operations

--- operators/operator.py
+++ operators/operator.py
@@ -1,6 +1,6 @@
 ############################### OPERATORS ###############################
 """
-# Arithmetic Operator (+, -, *, /, %)
+# Arithmetic Operator (+, -, *, /, %) [x]
 # Relational Operator (<, >, <=, >=, !=, ==)
 # Logical Operator (AND, OR, NOT)
 # Bitwise Operator (&, |, ^, ~, <<, >>)
@@ -8,3 +8,15 @@
 # Ternary Operator (x if condition else y)
 # Identity Operator (is, is not)
 """
+
+############# Arithmetic Operator #############
+a = 15
+b = 4
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b) # returns floating(datatype) point value
+print("Floor Division:", a // b) # returns integer(datatype) value
+print("Modulo:", a % b)
+print("Exponentiation:", a ** b)
